3c756efc4775ec8e6d60728f21a4c65c1d355a14
MRMramees <rameesbassam@infotech.com>
2021-01-05 11:32:03 +0530

    purchasemulti

--- bi_purchasemulti/models/models.py
+++ bi_purchasemulti/models/models.py
@@ -13,27 +13,39 @@ class createpurchaseorder(models.TransientModel):
 
 	new_order_line_ids = fields.One2many('getsale.orderdata', 'new_order_line_id',String="Order Line")
 	date_order = fields.Datetime(string='Order Date', required=True, copy=False, default=fields.Datetime.now)
-	mycompany_id = fields.Many2one('res.company', string='Company', required = True)
+	# mycompany_id = fields.Many2one('res.company', string='Company', required = True)
 
 	@api.model
 	def default_get(self,  default_fields):
 		res = super(createpurchaseorder, self).default_get(default_fields)
 		data = self.env['sale.order'].browse(self._context.get('active_ids',[]))
+		company = data.company_id
 		update = []
 		for record in data.order_line:
-			update.append((0,0,{
-							'product_id' : record.product_id.id,
-							'product_uom' : record.product_uom.id,
-							'order_id': record.order_id.id,
-							'name' : record.name,
-							'product_qty' : record.product_uom_qty,
-							'price_unit' : record.price_unit,
-							'product_subtotal' : record.price_subtotal,
-							}))
+			quant_id = self.env['stock.quant'].search([('product_id','=',record.product_id.id),('company_id','=',company.id),('quantity','>',record.product_uom_qty)]) 
+			if quant_id.id==False:
+				update.append((0,0,{
+								'product_id' : record.product_id.id,
+								'product_uom' : record.product_uom.id,
+								'order_id': record.order_id.id,
+								'name' : record.name,
+								'product_qty' : record.product_uom_qty,
+								'price_unit' : record.price_unit,
+								'product_subtotal' : record.price_subtotal,
+								}))
 		res.update({'new_order_line_ids':update})
 		return res
 
 	def action_create_purchase_order(self):
+		#company auto selection----
+		data = self.env['sale.order'].browse(self._context.get('active_ids',[]))
+		company=data.company_id
+		view_context = self.env.context
+		allowed_companies = view_context.get('allowed_company_ids', False)
+		for i in allowed_companies:
+			if i!= company.id:
+				self.mycompany_id=self.env['res.company'].search([('id','=',i)])
+		#-----
 		purchase_res = self.env['purchase.order']
 		sale_res = self.env['sale.order']
 		warehouse_res = self.env['stock.warehouse'].search([('name','=',self.mycompany_id.name)])
@@ -117,41 +129,47 @@ class bi_purchasemulti(models.Model):
 
 	def action_confirm(self):
 		company = self.company_id
+		records = []
 		
 		for rec in self.order_line:
 			quant_id = self.env['stock.quant'].search([('product_id','=',rec.product_id.id),('company_id','=',company.id),('quantity','>',rec.product_uom_qty)])
-			
-			if len(quant_id)==1:
+			records.append(quant_id.id)
+		flag = 0
+		for i in records:
+				if i == False:
+					flag = 1
+					if flag ==1:
 
-				
-				if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-					raise UserError(_(
-						'It is not allowed to confirm an order in the following states: %s'
-					) % (', '.join(self._get_forbidden_state_confirm())))
-				
-				for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-					order.message_subscribe([order.partner_id.id])
-				self.write({
-					'state': 'sale',
-					'date_order': fields.Datetime.now()
-				})
+						#wizard function call 'purchase.order' when there is no stock
+						return {  
+							'name': _('Journal Creation'),
+							'type': 'ir.actions.act_window',
+							'res_model': 'create.purchaseorder',
+							'view_mode': 'form',
+							'target': 'new',
+						}
+					
+					else :
+						if self._get_forbidden_state_confirm() & set(self.mapped('state')):
+							raise UserError(_(
+								'It is not allowed to confirm an order in the following states: %s'
+							) % (', '.join(self._get_forbidden_state_confirm())))
+						
+						for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
+							order.message_subscribe([order.partner_id.id])
+						self.write({
+							'state': 'sale',
+							'date_order': fields.Datetime.now()
+						})
 
-				context = self._context.copy()
-				context.pop('default_name', None)
+						context = self._context.copy()
+						context.pop('default_name', None)
 
-				self.with_context(context)._action_confirm()
-				if self.env.user.has_group('sale.group_auto_done_setting'):
-					self.action_done()
-				return True
-			else :
-				#wizard function call 'purchase.order' when there is no stock
-				return {  
-        			'name': _('Journal Creation'),
-            		'type': 'ir.actions.act_window',
-            		'res_model': 'create.purchaseorder',
-            		'view_mode': 'form',
-            		'target': 'new',
-    			}
+						self.with_context(context)._action_confirm()
+						if self.env.user.has_group('sale.group_auto_done_setting'):
+							self.action_done()
+						return True
+					
 
 
 #-----------------------------------------------------------------------------------------------
